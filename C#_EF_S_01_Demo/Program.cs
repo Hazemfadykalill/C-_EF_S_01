7c2685e1df5f80844044b2a2ed6bdcb5f604e423
Hazem Fady <hazemfady123@gmail.com>
2024-08-22 14:05:42 +0300

    Add project files.

---
+++ C#_EF_S_01_Demo/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace C__EF_S_01_Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
